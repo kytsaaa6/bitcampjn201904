f6ab8f1ea85c0b850c515b368dcc2e2768c52e06
kytsaaa6 <kytsaaa6@naver.com>
2019-08-13 18:39:02 +0900

    Rest API - JAXB2 를 이용한 XML 처리 실습

---
+++ Spring/Spring-MVC/src/main/java/com/bitcamp/mvc/guestListController.java
@@ -0,0 +1,33 @@
+package com.bitcamp.mvc;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bitcamp.mvc.domain.GuestMessage;
+import com.bitcamp.mvc.domain.GuestMessageList;
+
+@Controller
+public class guestListController {
+
+	@RequestMapping("/guest/xmlList.xml")
+	@ResponseBody
+	public GuestMessageList xmlList() {
+		
+		return getmessageList();
+	}
+
+	private GuestMessageList getmessageList() {
+		
+		List<GuestMessage> message = new ArrayList<GuestMessage>();
+		message.add(new GuestMessage(1, "안녕하세요", new Date()));
+		message.add(new GuestMessage(2, "반갑습니다", new Date()));
+		message.add(new GuestMessage(3, "또 만나요", new Date()));
+		
+		return new GuestMessageList(message);
+	}
+}
